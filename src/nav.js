071fa4dd1f7c0579dee027469762c332db3e995d
kaki li <kaki.noodles@gmail.com>
2020-05-21 18:39:47 +0800

    fixed nav mobile

--- src/nav.js
+++ src/nav.js
@@ -21,21 +21,21 @@ const Nav =() => {
     return (
       <div >
         <div className="nav vw-100  bg-white d-flex justify-content-between position-fixed fixed-top  px-5-lg  pt-2 align-items-center">
-          <div className="d-lg-block d-md-block d-sm-none d-none p-4 ml-4 nav-burger" onClick={()=> setNavOpen(!isNavOpen)} >三</div>
+          <div className="d-block p-4 ml-4 nav-burger" onClick={()=> setNavOpen(!isNavOpen)} >三</div>
 
-            <div className="d-lg-flex d-md-flex d-none justify-content-start " >
+       {/*     <div className="d-lg-flex d-md-flex d-sm-flex justify-content-start " >
               <div className=" d-flex justify-content-end px-5 w-100 " >
-                <a className=" nav-item  h5 px-4" href="#home">Home</a>
+                <a className=" nav-item  h5 px-4" href="#home">Homee</a>
                 <a className=" nav-item  h5 px-4" href="#about">About</a>
                 <a className=" nav-item  h5 px-4" href="#skill">Skill</a>
                 <a className=" nav-item  h5 px-4" href="#work">Work</a>
                 <a className=" nav-item  h5 px-4" href="#contact">Contact</a>
               </div>
-            </div>
+            </div>*/}
 
 
 
-          <div className="d-lg-none d-md-none d-block p-3 ">
+         {/* <div className="d-lg-none d-md-none d-block p-3">
             <div className="dropdown  ">
               <div className="dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 三
@@ -48,9 +48,9 @@ const Nav =() => {
                 <a className="dropdown-item pt-5 pb-5 h5" href="#contact">Contact</a>
               </div>
             </div>
-          </div>
+          </div>*/}
         </div>
-        <div className="w-100 d-flex justify-content-start" >
+        <div className="w-100 d-flex justify-content-lg-start justify-content-md-start justify-content-sm-center" >
           <NavProject style={navAnime}/>
         </div>
       </div>
--- src/nav_project.js
+++ src/nav_project.js
@@ -13,11 +13,11 @@ const NavProject = ({ style }) => {
       <animated.div className='nav-wrapper' style={ style }>
 
           <div className="nav-box">
-            <div className = "projects"> Projects</div>
-            <Link to={`/plantopia`} className="pro-nav d-block pt-5"> Wechat Miniprogram</Link>
-            <Link to={`/spareleash`} className="pro-nav d-block pt-5"> WebApp</Link>
-            <Link to={`/cocktail`} className="pro-nav d-block pt-5"> Ruby on Rails</Link>
-            <Link to={`/fun`} className="pro-nav d-block pt-5"> JS projects</Link>
+            <div className = "projects text-center"> Projects</div>
+            <Link to={`/plantopia`} className="pro-nav text-center d-block pt-5"> Wechat Miniprogram</Link>
+            <Link to={`/spareleash`} className="pro-nav text-center d-block pt-5"> WebApp</Link>
+            <Link to={`/cocktail`} className="pro-nav text-center d-block pt-5"> Ruby on Rails</Link>
+            <Link to={`/fun`} className="pro-nav text-center d-block pt-5"> JS projects</Link>
           </div>
 
       </animated.div>
