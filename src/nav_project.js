e6c4a55dc68fb85ce6add65c045d636f23d552bd
kaki li <kaki.noodles@gmail.com>
2020-05-22 12:12:12 +0800

    nav position on destop adjusted

--- src/nav_project.js
+++ src/nav_project.js
@@ -14,7 +14,7 @@ const NavProject = ({ style }) => {
 
           <div className="nav-box">
             <div className = "projects text-center"> Projects</div>
-            <Link to={`/plantopia`} className="pro-nav text-center d-block pt-5"> Wechat Miniprogram</Link>
+            <Link to={`/plantopia`} className="pro-nav text-center d-block pt-5"> Wechat <br /> Miniprogram</Link>
             <Link to={`/spareleash`} className="pro-nav text-center d-block pt-5"> WebApp</Link>
             <Link to={`/cocktail`} className="pro-nav text-center d-block pt-5"> Ruby on Rails</Link>
             <Link to={`/fun`} className="pro-nav text-center d-block pt-5"> JS projects</Link>
